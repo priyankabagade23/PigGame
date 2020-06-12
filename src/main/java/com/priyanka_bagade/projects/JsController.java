296fa3a646f33b4734b43d41c6c54cf7f728307e
priyanka bagade <priyabagade21997@gmail.com>
2020-06-12 20:24:01 +0530

    changed index to piggame

--- src/main/java/com/priyanka_bagade/projects/JsController.java
+++ src/main/java/com/priyanka_bagade/projects/JsController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JsController {
 
-    @RequestMapping(value = "/index")
+    @RequestMapping(value = "/piggame")
     public String getIndex(){
-        return "index";
+        return "piggame";
     }
 
 
